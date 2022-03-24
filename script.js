5711f85efb5a02824834af46b1f0d25c7b27cf32
jullisshh <jullisshh@mail.ru>
2022-03-24 20:04:33 +0700

    Adding script

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Lesson01";
+let screens = "ScreenName";
+let screenPrice = "3000$";
+const rollback = "rollback";
+let fullPrice = "fullPrice";
+const adaptive = "adaptive";
+
+alert ("Извините, если есть ошибки♥");
+
+console.log("Хорошего дня!");
